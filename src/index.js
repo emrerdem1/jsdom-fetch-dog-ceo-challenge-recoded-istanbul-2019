e1b8d5855bd9a72391574e0924a974107f8438e9
emrerdem1 <emrerdem94@gmail.com>
2020-04-18 12:37:38 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", function () {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    const imgcon = document.getElementById('dog-image-container');
+    const dogB = document.getElementById('dog-breeds');
+
+    fetch(imgUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            //challange 1
+            console.log(json);
+            json.message.forEach(item => {
+                let tempImg = new Image();
+                tempImg.src = item;
+                imgcon.appendChild(tempImg);
+            });
+        })
+
+    fetch(breedUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            //challange 2
+            for (let key of Object.keys(json.message)) {
+                let dogBName = document.createElement("li");
+                dogBName.appendChild(document.createTextNode(key));
+                dogB.appendChild(dogBName);
+            }
+            //challange 3
+            document.querySelectorAll("li").forEach(function (e) {
+                e.addEventListener('click', function () {
+                    this.style.color = "red";
+                })
+            });
+
+        })
+
+    //challange 4 - not yet finished
+    //Object.keys(..).sort()
+    const dropsort = document.getElementById('breed-dropdown');
+    const allLI = document.querySelectorAll("li");
+    let dropArr = ['a', 'b', 'c', 'd'];
+    allLI.forEach(item => {
+        if (item.textContent[0] === dropsort.value);
+    })
+
+})
